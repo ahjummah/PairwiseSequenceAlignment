a6fd851e53a2c07b5b5f09cbdda0d8acfcc3071d
jessacabigas <jessacabigas@gmail.com>
2016-12-05 16:24:51 +0800

    Updated Nucleotides Sequence

--- src/UI.java
+++ src/UI.java
@@ -732,15 +732,15 @@ public class UI extends javax.swing.JFrame {
                 if (seq1.charAt(l) == '-' || seq2.charAt(l) == '-') {
                     fw.write(" ");
                     dif += Integer.parseInt(gap_tf.getText());
-                    System.out.println(Integer.parseInt(gap_tf.getText()));
+//                    System.out.println(Integer.parseInt(gap_tf.getText()));
                 } else if (seq1.charAt(l) == seq2.charAt(l)) {
                     fw.write("|");
                     score += Integer.parseInt(match_tf.getText());
-                    System.out.println(Integer.parseInt(match_tf.getText()));
+//                    System.out.println(Integer.parseInt(match_tf.getText()));
                 } else {
                     fw.write(" ");
                     score += Integer.parseInt(mismatch_tf.getText());
-                    System.out.println(Integer.parseInt(mismatch_tf.getText()));
+//                    System.out.println(Integer.parseInt(mismatch_tf.getText()));
                 }
             }
 
